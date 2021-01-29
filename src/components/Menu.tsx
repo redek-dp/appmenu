d4378c28889072a04607ac6b89d3b3c757518f9e
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2021-01-28 22:48:24 -0300

    1.6

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,11 +7,8 @@ import {
 } from '@ionic/react';
 
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-
 
 const Menu: React.FC = () => {
-  const location = useLocation();
 
   return (
     <IonMenu contentId="main" type="overlay">
